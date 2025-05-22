41fc4afed739c05da5c38580f389f0c2f71f9672
jared <mcjey103@gmail.com>
2025-05-22 10:18:09 +0300

    Added user input handling to main.c

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, world!\n");
+    char name[50];
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("Hello, %s!\n", name);
     return 0;
 }
